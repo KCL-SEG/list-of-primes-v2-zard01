bc9764dc38fa6f22714935a911482b4cecefec81
zard01 <areesha.durrani@hotmail.com>
2022-10-05 19:12:33 +0100

    Throw Value Error
    Check if number_of_primes value is less than 1. If so, throw error, otherwise continue to create list of first number_of_primes prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,29 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list =  []
+  if number_of_primes<1:
+    raise ValueError("ValueError exception thrown")
+  else:
+    list = [2]
+    numToTest=2
+    while len(list)!=number_of_primes:
+      numToTest+=1
+  
+      isPrime=True
+  
+      if numToTest%2:
+        for i in range (3,(numToTest+1)//2,2):
+          if numToTest%i==0: 
+            isPrime=False
+            break
+        
+        if isPrime:
+          list.append(numToTest)
+  
     return list
+  
+  
+  	
+  
+
+
