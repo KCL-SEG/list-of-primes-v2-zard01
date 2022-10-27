dccce6a0ec3b4dd545a7b2154f49512747ba70ab
Areesha <areesha.durrani@hotmail.com>
2022-10-27 16:24:15 +0100

    Add isPrime function

--- primes.py
+++ primes.py
@@ -1,6 +1,15 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(numToTest):
+    primeFlag=True
+    if numToTest%2:
+      for i in range (3,(numToTest+1)//2,2):
+        if numToTest%i==0:
+          primeFlag=False
+          break
+    return primeFlag
+
 def primes(number_of_primes):
   if number_of_primes<1:
     raise ValueError("ValueError exception thrown")
@@ -8,23 +17,7 @@ def primes(number_of_primes):
     list = [2]
     numToTest=2
     while len(list)!=number_of_primes:
-      numToTest+=1
-  
-      isPrime=True
-  
-      if numToTest%2:
-        for i in range (3,(numToTest+1)//2,2):
-          if numToTest%i==0: 
-            isPrime=False
-            break
-        
-        if isPrime:
+        numToTest+=1
+      if isPrime(numToTest):
           list.append(numToTest)
-  
     return list
-  
-  
-  	
-  
-
-
