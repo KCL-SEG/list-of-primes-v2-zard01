1c170a7af93de6afffc28e468e40d553b06cb36c
Areesha <areesha.durrani@hotmail.com>
2022-10-27 16:27:01 +0100

    Fix indentation error

--- primes.py
+++ primes.py
@@ -4,20 +4,20 @@
 def isPrime(numToTest):
     primeFlag=True
     if numToTest%2:
-      for i in range (3,(numToTest+1)//2,2):
-        if numToTest%i==0:
-          primeFlag=False
-          break
+        for i in range (3,(numToTest+1)//2,2):
+            if numToTest%i==0:
+                primeFlag=False
+                break
     return primeFlag
 
 def primes(number_of_primes):
-  if number_of_primes<1:
-    raise ValueError("ValueError exception thrown")
-  else:
-    list = [2]
-    numToTest=2
-    while len(list)!=number_of_primes:
-        numToTest+=1
-      if isPrime(numToTest):
-          list.append(numToTest)
+    if number_of_primes<1:
+        raise ValueError("ValueError exception thrown")
+    else:
+        list = [2]
+        numToTest=2
+        while len(list)!=number_of_primes:
+            numToTest+=1
+            if isPrime(numToTest):
+                list.append(numToTest)
     return list
